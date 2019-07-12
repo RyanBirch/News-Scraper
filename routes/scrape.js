3fe3be2034aaccfcb9a48d06f8c3d671f785c86d
Ryan Birch <ryan.birch@gmail.com>
2019-07-12 02:33:55 -0400

    created models and inserted documents

--- routes/scrape.js
+++ routes/scrape.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 
 // database
-// const db = require('../models')
+const db = require('../models')
 
 async function scrape() {
   // get data from news site
@@ -14,38 +14,26 @@ async function scrape() {
   $('.headline').each(function(i, elem) {
     let result = {}
 
-    // result.title = $(this).children('a').text()
-    // // if (result.title) console.log(result.title)
-    // console.log(result.title)
-
-    // result.link = $(this).children('a').attr('href')
-    // // if (result.link) console.log(result.link)
-    // console.log(result.link)
-
-    // if ($(this).siblings('.row').find('.blurb').text())
-    //   result.summary = $(this).siblings('.row').find('.blurb').text()
-    // else
-    //   result.summary = $(this).siblings('.blurb').text()
-    
-    // // if (result.summary) console.log(result.summary)
-    // console.log(result.summary)
-    // console.log(`\n`)
-
-
-
     result.title = $(this).children('a').text()
     result.link = $(this).children('a').attr('href')
 
-    if ($(this).siblings('.row').find('.blurb').text())
+    if ($(this).siblings('.row').find('.blurb').text()) {
       result.summary = $(this).siblings('.row').find('.blurb').text()
-    else
+    } else {
       result.summary = $(this).siblings('.blurb').text()
+    }
 
-      if (result.title && result.link && result.summary) {
-        console.log(result.title)
-        console.log(result.link)
-        console.log(result.summary + '\n\n')
-      }
+    if (result.title && result.link && result.summary) {
+      // console.log(result.title)
+      // console.log(result.link)
+      // console.log(result.summary + '\n\n')
+
+      db.Article.create({
+        title: result.title,
+        link: result.link,
+        summary: result.summary
+      })
+    }
   })
 }
 
